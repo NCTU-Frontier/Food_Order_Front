8351ff23ff516e99cc226cad4395ab687f313657
Gavin Lee <sz110010@gmail.com>
2016-10-03 10:11:49 +0800

    Add body component
    Body component for the main part of web page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		NavComponent,
+		BodyComponent,
 	],
 	imports: [
 		BrowserModule,
---
+++ src/app/body/body.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BodyComponent } from './body.component';
+
+describe('Component: Body', () => {
+  it('should create an instance', () => {
+    let component = new BodyComponent();
+    expect(component).toBeTruthy();
+  });
+});
